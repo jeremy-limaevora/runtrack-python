c1c0f7fe8862541e0f2ac24f807c451a03b80db8
jeremy-limaevora <146709075+jeremy-limaevora@users.noreply.github.com>
2023-12-01 12:02:51 +0100

    Add files via upload

---
+++ jour5/Job05/job5.py
@@ -0,0 +1,16 @@
+alph = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+def position_alph(lettre):
+    return alph.find(lettre)
+
+def jc(message, decalage):
+    resultat = ''
+    for lettre in message.upper():
+        if lettre in alph:
+            indice = (position_alph(lettre) + decalage) % 26
+            resultat += alph[indice]
+        else:
+            resultat += lettre
+    return resultat
+
+print(jc('Bonjour tous le monde ', 2))
