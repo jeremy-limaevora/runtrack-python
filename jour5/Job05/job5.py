d23bd3a30e3b980bcc3389c4f30ff286e0aa3153
jeremy-limaevora <146709075+jeremy-limaevora@users.noreply.github.com>
2023-12-01 10:32:09 +0100

    Update job5.py

--- jour5/Job05/job5.py
+++ jour5/Job05/job5.py
@@ -3,14 +3,14 @@ alph = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 def position_alph(lettre):
     return alph.find(lettre)
 
-def cesar(message, decalage):
+def jc(message, decalage):
     resultat = ''
     for lettre in message:
         if lettre in alph:
-            indice = (position_alphabet(lettre) + decalage) % 26
+            indice = (position_alph(lettre) + decalage) % 26
             resultat += alph[indice]
         else:
             resultat += lettre
     return resultat
 
-print(cesar('Bonjour tous le monde ', 2))
+print(jc('Bonjour tous le monde ', 2))
